4e91946ccade93fed64543767aca4b442f551ab9
Arthur Smid <smidarthur@gmail.com>
2025-07-15 15:53:13 -0700

    move code to script at bottom of html to end FOUC

--- main.js
+++ main.js
@@ -27,14 +27,3 @@ if (typeof jQuery !== "undefined") {
     }
   })
 }
-window.addEventListener("load", (event) => {
-
-  const emailLinks =
-  document.querySelectorAll('a[href^="mailto:"]');
-  
-  emailLinks.forEach(link => {
-  console.log(link.innerText);
-  link.href = link.href + link.innerText;
-  });
-  
-});
