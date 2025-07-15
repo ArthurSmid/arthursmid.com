948eccf41576f751b29f70541291da67fea482fb
Arthur Smid <smidarthur@gmail.com>
2025-07-15 14:29:46 -0700

    new contact page

--- main.js
+++ main.js
@@ -27,3 +27,14 @@ if (typeof jQuery !== "undefined") {
     }
   })
 }
+window.addEventListener("load", (event) => {
+
+  const emailLinks =
+  document.querySelectorAll('a[href^="mailto:"]');
+  
+  emailLinks.forEach(link => {
+  console.log(link.innerText);
+  link.href = link.href + link.innerText;
+  });
+  
+});
